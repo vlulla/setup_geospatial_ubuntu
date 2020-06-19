cc23e42cdcb3c2b4d51d58fc2e64d83042ab83f3
Vijay Lulla <vijaylulla@gmail.com>
2020-06-19 15:09:26 -0400

    added a pkgs that I use regularly

--- setup_geospatial_env_aws.sh
+++ setup_geospatial_env_aws.sh
@@ -17,9 +17,9 @@ sudo apt-get install --yes --auto-remove r-base r-base-dev r-recommended littler
 [ -d "/usr/lib/R/site-library/littler" ] && [ ! $(echo ${PATH} | grep "/usr/lib/R/site-library/littler") ] && PATH="${PATH}:/usr/lib/R/site-library/littler/bin:/usr/lib/R/site-library/littler/examples"
 mkdir -p ${HOME}/.R && [ ! -f "${HOME}/.R/Makevars" ] && touch ${HOME}/.R/Makevars && echo "MAKEFLAGS += -j" >> ${HOME}/.R/Makevars
 
-sudo R --vanilla --no-save --no-restore -e "options(repos='https://cloud.r-project.org/');install.packages(c('docopt','BiocManager'),dependencies=TRUE)"
+sudo R --quiet --vanilla --no-save --no-restore -e "options(repos='https://cloud.r-project.org/');install.packages(c('docopt','BiocManager'),dependencies=TRUE)"
 sudo $(which installBioc.r) graph # one of the below packages needs it...
-sudo $(which install2.r) --deps TRUE --error --ncpus $(nproc) --skipinstalled RSQLite ggplot2 igraph rbenchmark data.table simstudy fst e1071 sf rgdal sp raster caret randomForest xgboost
+sudo $(which install2.r) --deps TRUE --error --ncpus $(nproc) --skipinstalled RSQLite ggplot2 igraph rbenchmark data.table simstudy fst e1071 sf rgdal sp raster lidR RPostgres caret randomForest xgboost
 
 ## ## Uncomment below if you want rstudio server on this instance
 ## gpg --keyserver keys.gnupg.net --recv-keys 3F32EE77E331692F
