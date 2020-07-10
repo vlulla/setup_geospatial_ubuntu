9604490a84e5c12162cb14ce953dbf730bd0c370
Vijay Lulla <vijaylulla@gmail.com>
2020-07-10 10:21:13 -0400

    Quoting parameters to prevent word splitting.
    These were recommended by https://www.shellcheck.net/

--- setup_geospatial_env_aws.sh
+++ setup_geospatial_env_aws.sh
@@ -14,12 +14,12 @@ sudo apt-get install --yes --auto-remove build-essential whois vim curl default-
 sudo apt-get install --yes --auto-remove texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra texinfo pandoc libudunits2-dev unixodbc-dev libgdal-dev
 sudo apt-get install --yes --auto-remove r-base r-base-dev r-recommended littler libfftw3-bin libfftw3-dev
 
-[ -d "/usr/lib/R/site-library/littler" ] && [ ! $(echo ${PATH} | grep "/usr/lib/R/site-library/littler") ] && PATH="${PATH}:/usr/lib/R/site-library/littler/bin:/usr/lib/R/site-library/littler/examples"
-mkdir -p ${HOME}/.R && [ ! -f "${HOME}/.R/Makevars" ] && touch ${HOME}/.R/Makevars && echo "MAKEFLAGS += -j" >> ${HOME}/.R/Makevars
+[ -d "/usr/lib/R/site-library/littler" ] && [ ! $(echo "${PATH}" | grep "/usr/lib/R/site-library/littler") ] && PATH="${PATH}:/usr/lib/R/site-library/littler/bin:/usr/lib/R/site-library/littler/examples"
+mkdir -p "${HOME}/.R" && [ ! -f "${HOME}/.R/Makevars" ] && touch "${HOME}/.R/Makevars" && echo "MAKEFLAGS += -j" >> "${HOME}/.R/Makevars"
 
 sudo R --quiet --vanilla --no-save --no-restore -e "options(repos='https://cloud.r-project.org/');install.packages(c('docopt','BiocManager'),dependencies=TRUE)"
-sudo $(which installBioc.r) graph EBImage # one of the below packages needs it...
-sudo $(which install2.r) --deps TRUE --error --ncpus $(nproc) --skipinstalled RSQLite ggplot2 igraph rbenchmark data.table simstudy fst e1071 sf rgdal sp raster lidR RPostgres caret randomForest xgboost vtreat
+sudo "$(which installBioc.r)" graph EBImage # one of the below packages needs it...
+sudo "$(which install2.r)" --deps TRUE --error --ncpus "$(nproc)" --skipinstalled RSQLite ggplot2 igraph rbenchmark data.table simstudy fst e1071 sf rgdal sp raster lidR RPostgres caret randomForest xgboost vtreat
 
 ## ## Uncomment below if you want rstudio server on this instance
 ## gpg --keyserver keys.gnupg.net --recv-keys 3F32EE77E331692F
